aed6bbd1161ba68d7fa6cafd2c4dee559e7bdcaa
Francsico Ossian <ossian.neto@gmail.com>
2025-06-02 22:37:57 -0300

    init

--- src/app/view/[id]/page.tsx
+++ src/app/view/[id]/page.tsx
@@ -1,12 +1,21 @@
 import { readProject } from "@/services/firebase/Read/readProject";
+import { Project } from "@/services/firebase/types";
 import EditorJSHTML from "editorjs-html";
+import { use } from "react";
 
 const edjsParser = EditorJSHTML();
 
-export default async function ViewPage({ params }: { params: { id: string } }) {
-  const project = await readProject(params.id);
+export default function ViewPage({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = use(params);
+  const project = readProject(id);
 
-  const blocks = project.content?.blocks ?? [];
+  const blocks =
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    ((project as unknown as Project)?.content as any)?.blocks ?? [];
   const html = edjsParser.parse({ blocks });
 
   return <div className="ml-4" dangerouslySetInnerHTML={{ __html: html }} />;
